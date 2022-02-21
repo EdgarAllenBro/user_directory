09e0a0398db8b8e20dc311a824ca3ef2e8ae86b6
Cameron Grieder <cjgrieder@gmail.com>
2022-02-21 16:30:11 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import data from './data'
+
+function FavList(props) {
+  const fav = props.fav;
+  const favList = fav.map((e,i)=> <li key={i}>{e}</li>)
+  return <ol>{favList}</ol>
+}
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const info = data
+  const [id,setId] = useState(1)
+  const user = info[id]
+  const [name,setName] = useState(`${user.name.first} ${user.name.last}`)
+  const [from,setFrom] = useState(`${user.city}, ${user.country}`)
+  const [employer,setEmployer] = useState(`${user.employer}`)
+  const [job, setJob] = useState(`${user.title}`)
+  const [favs,setFavs] = useState([user.favoriteMovies])
+
+
+  return(
+<div>
+
+  <div className='userInfo'>
+    <h1>{name}</h1>
+    <h3>From:{from}</h3>
+    <h3>Job Title:{job}</h3>
+    <h3>Employer:{employer}</h3>
+    <h2>Favorite Movies</h2>
+    <FavList fav={favs} />
+    
+  </div>
+</div>
+  )
 }
 
 export default App;
